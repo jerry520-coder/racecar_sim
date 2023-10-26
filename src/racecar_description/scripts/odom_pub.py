4d2d31db2bc10ff90e588a04b221f19ee97870f8
binghe <binghe@orbbec.com>
2023-10-26 17:54:31 +0800

    2

--- src/racecar_description/scripts/odom_pub.py
+++ src/racecar_description/scripts/odom_pub.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
 import rospy
 import math
@@ -12,15 +12,17 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TransformStamped
 
 # 定义部分参数
-frame_name  = "odom"
+frame_name = "odom"
 child_frame_name = "base_footprint"
-pub_name    = "odom_topic"
-imu_name    = "imu_data"
-joint_states_name   = "/racecar/joint_states"
+pub_name = "odom_topic"
+imu_name = "imu_data"
+joint_states_name = "/racecar/joint_states"
 # 定义起始坐标
-start_x     = -0.5
-start_y     = 0
-start_z     = 0
+start_x = -0.5
+start_y = 0
+start_z = 0
+
+
 class odom_class:
     odom_topic = Odometry()
     odom_tf = TransformStamped()
@@ -28,15 +30,18 @@ class odom_class:
     yaw_angle = 0
     old_yaw_angle = 0
     old_position = 0
+
     # 微分线速度
-    #velocity = 0
+    # velocity = 0
     def __init__(self):
         # 订阅 IMU 传感器（陀螺仪）
         rospy.Subscriber(imu_name, Imu, self.callback_imu, queue_size=1)
         # 订阅 关节
-        rospy.Subscriber(joint_states_name, JointState, self.callback_join, queue_size=1)
+        rospy.Subscriber(
+            joint_states_name, JointState, self.callback_join, queue_size=1
+        )
         # 发布 odom 坐标
-        self.pub = rospy.Publisher(pub_name, Odometry, queue_size = 1)
+        self.pub = rospy.Publisher(pub_name, Odometry, queue_size=1)
         # 发布 tf odom 到 odom_footprint
         self.br = tf2_ros.TransformBroadcaster()
         # 定义时间差
@@ -56,9 +61,15 @@ class odom_class:
 
     def callback_imu(self, data):
         # 正交分解 偏航角（必须）
-        #（在起始位置：以中线x轴为分界线，左正半y轴为正，右负半y轴为负，超过负半x轴直接跳变正负最大值）
+        # （在起始位置：以中线x轴为分界线，左正半y轴为正，右负半y轴为负，超过负半x轴直接跳变正负最大值）
         self.yaw_angle = euler_from_quaternion(
-            (data.orientation.x,data.orientation.y,data.orientation.z,data.orientation.w))
+            (
+                data.orientation.x,
+                data.orientation.y,
+                data.orientation.z,
+                data.orientation.w,
+            )
+        )
         # 四元数 xy轴角度（四元数，必须）
         self.odom_topic.pose.pose.orientation.x = data.orientation.x
         self.odom_topic.pose.pose.orientation.y = data.orientation.y
@@ -68,7 +79,7 @@ class odom_class:
         self.odom_topic.twist.twist.angular.x = data.angular_velocity.x
         self.odom_topic.twist.twist.angular.y = data.angular_velocity.y
         self.odom_topic.twist.twist.angular.z = data.angular_velocity.z
-        #self.pub.publish(self.odom_topic)
+        # self.pub.publish(self.odom_topic)
 
     def callback_join(self, data):
         # 计算时间差
@@ -78,26 +89,32 @@ class odom_class:
         # 记录时间
         self.odom_topic.header.stamp = rospy.Time.now()
         self.odom_tf.header.stamp = rospy.Time.now()
-        #try:
+        # try:
         # 编码器 测速
-        lrw_topic = data.name.index('left_rear_axle')
-        rrw_topic = data.name.index('right_rear_axle')
+        lrw_topic = data.name.index("left_rear_axle")
+        rrw_topic = data.name.index("right_rear_axle")
         # 平均速度
-        velocity = (data.velocity[lrw_topic] + data.velocity[rrw_topic])/(2 * 13.95348)
+        velocity = (data.velocity[lrw_topic] + data.velocity[rrw_topic]) / (
+            2 * 13.95348
+        )
         # 平均路程
-        position = velocity * add_time_joint 
+        position = velocity * add_time_joint
         #  xy轴坐标(位置，必须)
-        self.odom_topic.pose.pose.position.x += (position*math.cos(self.yaw_angle[2]))
-        self.odom_topic.pose.pose.position.y += (position*math.sin(self.yaw_angle[2]))
+        self.odom_topic.pose.pose.position.x += position * math.cos(self.yaw_angle[2])
+        self.odom_topic.pose.pose.position.y += position * math.sin(self.yaw_angle[2])
         self.odom_topic.pose.pose.position.z += 0
         # 正交分解 xy轴速度（线速度，必须）
         # 速度是相对小车的，往前是正的，往后是负的.
-        if ((self.yaw_angle[2] < 1.52) and (self.yaw_angle[2] > -1.52)):
-            self.odom_topic.twist.twist.linear.x = velocity*math.cos(self.yaw_angle[2])
-        else :
-            self.odom_topic.twist.twist.linear.x = -velocity*math.cos(self.yaw_angle[2])
-    
-        self.odom_topic.twist.twist.linear.y = velocity*math.sin(self.yaw_angle[2])
+        if (self.yaw_angle[2] < 1.52) and (self.yaw_angle[2] > -1.52):
+            self.odom_topic.twist.twist.linear.x = velocity * math.cos(
+                self.yaw_angle[2]
+            )
+        else:
+            self.odom_topic.twist.twist.linear.x = -velocity * math.cos(
+                self.yaw_angle[2]
+            )
+
+        self.odom_topic.twist.twist.linear.y = velocity * math.sin(self.yaw_angle[2])
         self.odom_topic.twist.twist.linear.z = 0
 
         self.pub.publish(self.odom_topic)
@@ -112,11 +129,12 @@ class odom_class:
         self.odom_tf.transform.rotation.w = self.odom_topic.pose.pose.orientation.w
         self.br.sendTransform(self.odom_tf)
 
-        
+
 def main():
     rospy.init_node("odom_imu")
     node = odom_class()
     rospy.spin()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- src/racecar_description/scripts/test/nav_sim_test.py
+++ src/racecar_description/scripts/test/nav_sim_test.py
@@ -4,19 +4,21 @@ import std_msgs.msg
 from ackermann_msgs.msg import AckermannDrive
 from geometry_msgs.msg import Twist
 
-import time 
+import time
 import threading
-pub = rospy.Publisher("/ackermann_cmd_mux/output", AckermannDrive,queue_size=1)
+
+pub = rospy.Publisher("/ackermann_cmd_mux/output", AckermannDrive, queue_size=1)
+
 
 def thread_job():
     rospy.spin()
 
+
 def callback(data):
-    speed = data.linear.x 
+    speed = data.linear.x
     turn = data.angular.z
 
     msg = AckermannDrive()
-
 
     msg.speed = speed
     msg.acceleration = 1
@@ -26,14 +28,15 @@ def callback(data):
 
     pub.publish(msg)
 
+
 def SubscribeAndPublish():
-    rospy.init_node('nav_sim', anonymous=True)
-    rospy.Subscriber('cmd_vel', Twist, callback,queue_size=1,buff_size=52428800)
-    #rospy.Subscriber('cmd_vel', Twist, callback,queue_size=1,buff_size=52428800)
+    rospy.init_node("nav_sim", anonymous=True)
+    rospy.Subscriber("cmd_vel", Twist, callback, queue_size=1, buff_size=52428800)
+    # rospy.Subscriber('cmd_vel', Twist, callback,queue_size=1,buff_size=52428800)
     rospy.spin()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         SubscribeAndPublish()
     except rospy.ROSInterruptException:
